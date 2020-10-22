e0cd408d7b7ad56bffcab409f730f851fee03a29
Carlos <carlos.huaynates@tecsup.edu.pe>
2020-10-22 11:58:23 -0500

    inicios y nosotros

--- router.js
+++ router.js
@@ -1,56 +1,54 @@
 const fs = require('fs');
 
 const router = (req, res) => {
-  switch (req.url) {
+    switch (req.url) {
 
-    case '/':
-      fs.readFile('./templates/index.html', (err, html) => {
-        res.writeHead(200, { 'Content-type': 'text/html' });
-        res.write(html);
-        res.end();
-      });
-      break;
+        case '/inicio':
+            fs.readFile('./templates/index.html', (err, html) => {
+                res.writeHead(200, { 'Content-type': 'text/html' });
+                res.write(html);
+                res.end();
+            });
+            break;
 
-    case '/bienvenido':
-      fs.readFile('./templates/index.html', (err, html) => {
-        res.writeHead(200, { 'Content-type': 'text/html' });
-        res.write(html);
-        res.end();
-      });
-      break;
+        case '/nosotros':
+            fs.readFile('./templates/nosotros.html', (err, html) => {
+                res.writeHead(200, { 'Content-type': 'text/html' });
+                res.write(html);
+                res.end();
+            });
+            break;
 
-    case '/nosotros':
-      fs.readFile('./templates/index.html', (err, html) => {
-        res.writeHead(200, { 'Content-type': 'text/html' });
-        res.write(html);
-        res.end();
-      });
-      break;
+        case '/nosotros':
+            fs.readFile('./templates/index.html', (err, html) => {
+                res.writeHead(200, { 'Content-type': 'text/html' });
+                res.write(html);
+                res.end();
+            });
+            break;
 
-    case '/servicios':
-      fs.readFile('./templates/index.html', (err, html) => {
-        res.writeHead(200, { 'Content-type': 'text/html' });
-        res.write(html);
-        res.end();
-      });
-      break;
-    
-    case '/contactos':
-        fs.readFile('./templates/index.html', (err, html) => {
-          res.writeHead(200, { 'Content-type': 'text/html' });
-          res.write(html);
-          res.end();
-        });
-        break;
+        case '/servicios':
+            fs.readFile('./templates/index.html', (err, html) => {
+                res.writeHead(200, { 'Content-type': 'text/html' });
+                res.write(html);
+                res.end();
+            });
+            break;
 
-    default:
-      res.writeHead(404, { 'Content-Type': 'text/html' });
-      res.write('404');
-      res.end();
-      break;
-  }
+        case '/contactos':
+            fs.readFile('./templates/index.html', (err, html) => {
+                res.writeHead(200, { 'Content-type': 'text/html' });
+                res.write(html);
+                res.end();
+            });
+            break;
+
+        default:
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            res.write('404');
+            res.end();
+            break;
+    }
 };
 
 module.exports = router;
-
-
